9329521ea54b1b8d1fb3aaefd5c93bbc149a5122
yashaswini-mn <yashaswnimn26@gmail.com>
2025-03-06 16:22:43 +0530

    commit

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from base.models import BaseModel
+
 from django import forms
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
--- project/base/models.py
+++ project/base/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 # Create your models here.
-class BaseModel(models.Model):
+class BaseModel(models.Model, User):
     uuid=models.UUIDField(primary_key=True, editable=False, default=uuid.uuid1)
     created_at=models.DateTimeField(auto_now=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+    is_active=User.is_active
 
     class Meta:
         abstract=True
